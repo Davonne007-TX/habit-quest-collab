b38000158dec2a80035f1f6e29c00dc5ba9e5575
Davonne Vigil <davonnejv@gmail.com>
2025-03-11 17:34:55 -0500

    add components and remove starter code

---
+++ src/components/LegendaryTale.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LegendaryTale() {
+  return <div>LegendaryTale</div>;
+}
---
+++ src/components/PersonalQuest.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PersonalQuest() {
+  return <div>PersonalQuest</div>;
+}
