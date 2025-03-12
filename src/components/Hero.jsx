c51df025afcb882609400c157a8099f03ff67057
Davonne Vigil <davonnejv@gmail.com>
2025-03-11 19:22:45 -0500

    feature: mobile responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,26 +6,26 @@ export default function Hero() {
   const quest = `Mauris consequat erat nisi, id vestibulum erat lobortis ut. Quisque at mollis turpis, nec mollis est. Pellentesque ac ex mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit malesuada sapien et pellentesque.`;
 
   return (
-    <section className="flex flex-col md:flex-row justify-center items-center mt-20 md:mt-40">
-      <section className="flex flex-col md:flex-row max-w-6xl gap-4">
-        <section className="max-w-lg flex flex-col gap-4">
-          <h2 className="text-6xl font-bold leading-18">{title}</h2>
+    <section className="flex flex-col md:flex-row justify-center items-center gap-8 mt-20 md:mt-40">
+      <section className="flex flex-col justify-center items-center md:flex-row max-w-6xl gap-4">
+        <section className="max-w-xs md:max-w-lg flex flex-col gap-4">
+          <h2 className="text-2xl md:text-6xl font-bold ">{title}</h2>
           <p>{quest}</p>
 
           <section className="flex gap-8 mt-4">
             <Button
               btnTxt={"Get Started on your Quest ↗"}
-              btnStyles={"bg-black text-white p-4 rounded-md"}
+              btnStyles={"bg-black text-white p-2 rounded-md"}
             />
             <Button
               btnTxt={"Learn More"}
-              btnStyles={"p-4 border-1 rounded-md border-black"}
+              btnStyles={"p-2 border-1 rounded-md border-black"}
             />
           </section>
         </section>
         <Image
           image={"./images/transform.jpeg"}
-          imageStyles={"max-w-md md:max-lg lg:max-w-xl"}
+          imageStyles={"max-w-xs md:max-w-lg lg:max-w-xl"}
         />
       </section>
     </section>
