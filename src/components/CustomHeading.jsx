5d5e8702fa8244a29393bbe8bf9cfa32decaa7d0
Davonne Vigil <davonnejv@gmail.com>
2025-03-11 22:38:35 -0500

    add custom component for headings

---
+++ src/components/CustomHeading.jsx
@@ -0,0 +1,8 @@
+export default function CustomHeading({ heading, text }) {
+  return (
+    <section className="flex flex-col justify-center items-center mt-60">
+      <h3 className="text-3xl md:text-5xl font-bold text-center">{heading}</h3>
+      <p className="text-gray-400 text-xl md:text-2xl mt-4">{text}</p>
+    </section>
+  );
+}
--- src/components/PersonalQuest.jsx
+++ src/components/PersonalQuest.jsx
@@ -1,16 +1,9 @@
 import React from "react";
+import CustomHeading from "./CustomHeading";
 
 export default function PersonalQuest() {
-  return (
-    <>
-      <section className="flex flex-col justify-center items-center mt-60">
-        <h3 className="text-3xl md:text-5xl font-bold text-center">
-          Choose Your Character
-        </h3>
-        <p className="text-gray-400 text-xl md:text-2xl mt-4">
-          Select a character class that matches your personality and goals
-        </p>
-      </section>
-    </>
-  );
+  const character = "Choose Your Character";
+  const select =
+    "Select a character class that matches your personality and goals";
+  return <CustomHeading heading={character} text={select} />;
 }
