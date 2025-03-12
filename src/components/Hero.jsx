56f4f9aad142a5f17cd6f51a2682ae3fdb258d50
Davonne Vigil <davonnejv@gmail.com>
2025-03-11 19:28:38 -0500

    add line height for heros title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,15 @@ export default function Hero() {
   const title = "Turn Your Habits Into an Epic Hero's Journey";
   const quest = `Mauris consequat erat nisi, id vestibulum erat lobortis ut. Quisque at mollis turpis, nec mollis est. Pellentesque ac ex mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit malesuada sapien et pellentesque.`;
 
+  const transform = "./images/transform.jpeg";
+
   return (
     <section className="flex flex-col md:flex-row justify-center items-center gap-8 mt-20 md:mt-40">
       <section className="flex flex-col justify-center items-center md:flex-row max-w-6xl gap-4">
         <section className="max-w-xs md:max-w-lg flex flex-col gap-4">
-          <h2 className="text-2xl md:text-6xl font-bold ">{title}</h2>
+          <h2 className="text-2xl md:text-6xl font-bold md:leading-18">
+            {title}
+          </h2>
           <p>{quest}</p>
 
           <section className="flex gap-8 mt-4">
@@ -24,8 +28,8 @@ export default function Hero() {
           </section>
         </section>
         <Image
-          image={"./images/transform.jpeg"}
-          imageStyles={"max-w-xs md:max-w-lg lg:max-w-xl"}
+          image={transform}
+          imageStyles={"max-w-xs md:max-w-lg lg:max-w-xl rounded-md"}
         />
       </section>
     </section>
