e951054f5f1a6a9f3a63c10c4e8d13ee2fb138de
Davonne Vigil <davonnejv@gmail.com>
2025-03-19 23:28:32 -0500

    update margin top on Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ export default function Card({
   return (
     <section className="max-w-4xl shadow-md text-white md:max-w-6xl mt-10">
       <div className="border-1 p-4 max-w-sm md:max-w-md lg:max-w-sm h-80 rounded-xl bg-purple-500">
-        <div className="flex flex-col justify-center items-center text-center gap-4 leading-6 mt-8">
+        <div className="flex flex-col justify-center items-center text-center gap-4 leading-6 mt-4">
           <span className="text-6xl">{characterImage}</span>
           <h1 className="font-bold text-2xl">{character}</h1>
           <p className="">{characterDescription}</p>
