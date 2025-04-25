450b50467345b1a9ae852aa7490e84529e35f0e0
Davonne Vigil <davonnejv@gmail.com>
2025-04-25 14:35:22 -0500

    update styles

--- src/components/CustomHeading.jsx
+++ src/components/CustomHeading.jsx
@@ -1,7 +1,9 @@
 export default function CustomHeading({ heading, text }) {
   return (
     <section className="flex flex-col justify-center items-center mt-40">
-      <h3 className="text-3xl md:text-5xl font-bold text-center">{heading}</h3>
+      <h3 className="text-3xl md:text-5xl font-alfa font-bold text-center">
+        {heading}
+      </h3>
       <p className="text-gray-400 text-xl md:text-2xl mt-4 text-center max-w-4xl">
         {text}
       </p>
