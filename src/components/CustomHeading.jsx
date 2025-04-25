c4ef09574d7bc96720bc597ebeab6efcae50c8c4
Davonne Vigil <davonnejv@gmail.com>
2025-04-25 01:17:21 -0500

    thats a wrap

--- src/components/CustomHeading.jsx
+++ src/components/CustomHeading.jsx
@@ -1,6 +1,6 @@
 export default function CustomHeading({ heading, text }) {
   return (
-    <section className="flex flex-col justify-center items-center mt-20 md:mt-40">
+    <section className="flex flex-col justify-center items-center mt-40">
       <h3 className="text-3xl md:text-5xl font-bold text-center">{heading}</h3>
       <p className="text-gray-400 text-xl md:text-2xl mt-4 text-center max-w-4xl">
         {text}
