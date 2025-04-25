9235d1fd677ee0e493a70ef83814e5c9de58418f
Davonne Vigil <davonnejv@gmail.com>
2025-04-25 00:42:47 -0500

    add numbers for each card in Process.jsx, with styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,15 +2,14 @@ import Button from "./Button";
 import Image from "./Image";
 
 export default function Card({
+  personalizeCard,
+  processNumber,
   cardImage,
   alt,
   cardIcon,
-  imageStyles,
   cardHeading,
-
   cardDescription,
   descriptionStyles,
-  personalizeCard,
   btnTxt,
   btnStyles,
   backgroundColor,
@@ -18,9 +17,13 @@ export default function Card({
   return (
     <section className="max-w-7xl shadow-md text-white md:max-w-sm mt-10 flex">
       <div
-        className={`border-1 p-4 max-w-sm md:max-w-md h-80 rounded-xl ${backgroundColor}`}
+        className={`relative border-1 p-4 max-w-sm md:max-w-md h-80 rounded-xl ${backgroundColor} overflow-hidden`}
       >
-        <div className="flex flex-col justify-center items-center text-center gap-4">
+        <div className="absolute text-white text-9xl opacity-10 font-extrabold top-2 left-4 select-none z-0">
+          {processNumber}
+        </div>
+
+        <div className="relative z-10 flex flex-col justify-center items-center text-center gap-4">
           <div className={personalizeCard}>
             <span className="text-6xl">{cardIcon}</span>
             <Image image={cardImage} imageStyles="mx-auto" alt={alt} />
--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -3,24 +3,28 @@ import Card from "./Card";
 export default function Process() {
   const howQuestUnfolds = [
     {
+      processNumber: "1",
       process: "Creating Habits",
       processDescription:
         "Set up daily habits that align with your personal goals.",
       id: 1,
     },
     {
+      processNumber: "2",
       process: "Complete Tasks",
       processDescription:
         "Check off completed habits to earn XP and level up your character.",
       id: 2,
     },
     {
+      processNumber: "3",
       process: "Journal Reflections",
       processDescription:
         "Record your thoughts your experiences in your quest journal.",
       id: 3,
     },
     {
+      processNumber: "4",
       process: "Reveal Your Story",
       processDescription:
         "AI generates your hero's journey based on your progress. Share story on Social Media.",
@@ -30,16 +34,21 @@ export default function Process() {
 
   return (
     <section className="flex flex-col md:flex-col lg:flex-row justify-center items-center gap-20 py-14">
-      <ul className="flex flex-col justify-center items-center lg:flex-row gap-10 w-full">
+      <ul className="flex flex-col justify-center items-center lg:flex-row gap-10 w-full relative">
         {howQuestUnfolds.map((unfold) => (
-          <Card
-            key={unfold.id}
-            backgroundColor="bg-gradient-to-r from-purple-600 via-indigo-500 to-teal-400"
-            personalizeCard="flex flex-col mt-22"
-            cardHeading={unfold.process}
-            cardDescription={unfold.processDescription}
-            descriptionStyles="text-xl p-2"
-          />
+          <>
+            <div className="flex flex-col justify-center items-center">
+              <Card
+                key={unfold.id}
+                backgroundColor="bg-gradient-to-r from-purple-600 via-indigo-500 to-teal-400"
+                personalizeCard="flex flex-col mt-28"
+                processNumber={unfold.processNumber}
+                cardHeading={unfold.process}
+                cardDescription={unfold.processDescription}
+                descriptionStyles="text-xl p-2"
+              />
+            </div>
+          </>
         ))}
       </ul>
     </section>
